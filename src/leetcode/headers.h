78806a6aea54ff6814103763ca3fa6ebc05f3dae
xdminsy <xsy2293419066@gmail.com>
2021-12-09 14:07:50 +0800

    Add headers.h

---
+++ src/leetcode/headers.h
@@ -0,0 +1,43 @@
+#ifndef ALGORITHM_HEADERS_H
+#define ALGORITHM_HEADERS_H
+
+#include <bitset>
+#include <cstddef>
+#include <cstdlib>
+#include <ctime>
+#include <functional>
+#include <initializer_list>
+#include <tuple>
+#include <utility>
+#include <typeinfo>
+#include <cstdint>
+#include <limits>
+#include <cfloat>
+#include <cctype>
+#include <cstring>
+#include <string>
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <regex>
+
+using namespace std;
+
+#endif //ALGORITHM_HEADERS_H
